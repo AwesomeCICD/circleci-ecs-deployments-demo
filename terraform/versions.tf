9a97aeee57907c0538f7e42b7ca9481712f47158
Vijay Pandian <ananth.vijay@gmail.com>
2024-08-12 18:32:58 -0400

    fix incorrect variable assignment in CI job

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.61.0"
     }
-    circleci = {
-      source  = "kelvintaywl/circleci"
-      version = "1.0.1"
-    }
     local = {
       source  = "hashicorp/local"
       version = "2.5.1"
