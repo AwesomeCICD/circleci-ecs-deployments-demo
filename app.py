66156204a76b012bb50bcca2c78cd11b9ea4bb76
Vijay Pandian <ananth.vijay@gmail.com>
2024-08-12 19:13:31 -0400

    use updated format for container

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def home():
     # Retrieve environment variables
     env_var1 = os.getenv('ENV_VAR1')
     env_var2 = os.getenv('ENV_VAR2')
-    return f"Hello, World! ENV_VAR1: {env_var1}, ENV_VAR2: {env_var2}"
+    return (
+        f"Hello, World! "
+        f"The CIRCLE_BUILD_NUM used for building this container was: {env_var1}, "
+        f"The CIRCLE_SHA that generated this container was : {env_var2}"
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
